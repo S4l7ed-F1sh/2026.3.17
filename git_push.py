41bde7053af81b6af8e6c15066292a1cd6b4a897
S4l7ed-F1sh <93729440+S4l7ed-F1sh@users.noreply.github.com>
2026-03-20 18:58:32 +0800

    default commit

--- git_push.py
+++ git_push.py
@@ -1,7 +1,9 @@
 import subprocess
 
 try:
-    res = subprocess.check_output(["git", "push", "mastergit "], stderr=subprocess.STDOUT, text=True)
+    res = subprocess.check_output(["git", "commit", "-m", "\"default commit\""], stderr=subprocess.STDOUT, text=True)
+    print(res)
+    res = subprocess.check_output(["git", "push"], stderr=subprocess.STDOUT, text=True)
     print(res)
 except subprocess.CalledProcessError as e:
     print(f"Git push 失败，错误信息:\n{e.output}")
